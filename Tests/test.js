8dbf5ed99156847bb29e6c703edea569c48eb8a4
Adrián González <agonzalezencabo@gmail.com>
2025-02-25 11:20:22 +0100

    prueba test

---
+++ Tests/test.js
@@ -0,0 +1,7 @@
+const { describe, it } = require("node:test")
+const assert = require("assert")
+describe("tests de prueba", () => {
+    it("should pass", () => {
+        assert.equal(1, 1)
+    })
+})
